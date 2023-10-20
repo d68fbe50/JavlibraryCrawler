58448e266b0e20c6f9d8276e158df60a86217059
desonglll <lindesong666@163.com>
2023-10-20 09:10:58 +0800

    ...

--- config/arguments.py
+++ config/arguments.py
@@ -1,20 +1,44 @@
+from rich import print
+
+print("Hello, [bold red]World[/bold red]!")
+
 ids = [
     # 在这里添加演员id
-    "aadt6",
-    'aaoa4',
-    # 'abbds',
-    # 'abdaa',
-    # 'ae3so',
-    # "ae5q6",
-    # 'aebe6',
-    # 'aeceq',
-    # 'aejdi',
-    # 'aemco',
-    # 'aeyaq',
-    # 'afgda',
-    # 'afnqu',
-    # 'aygqk',
-    # 'azjsc'
+    "aadt6",  # 小鳥遊もえ
+    # 'aaoa4',  # 小湊よつ葉
+    # 'abbds',  # 山手梨愛
+    # 'ae3so',  # 本庄鈴
+    # "ae5q6",  # 楓カレン
+    # 'aebe6',  # 七沢みあ
+    # 'aeceq',  # 山岸逢花
+    # 'aejdi',  # 明里つむぎ
+    # 'aemco',  # 桜空もも
+    # 'aeyaq',  # 河北彩花
+    # 'afgda',  # 相沢みなみ
+    # 'afnqu',  # 坂道みる
+    # 'aygqk',  # 夏目彩春
+    # 'azjsc',  # 松嶋真麻
+    # "azhfk",  # 星野景子
+    # "aaivs",  # 神木麗
+    # "aalqo",  # 坂井なるは
+    # "abaew",  # 石川澪
+    # "abdaa",  # 楓芙愛
+    # "ae4ee",  # 星宮一花
+    # "aentc",  # 美谷朱里
+    # "aaouy",  # 新有菜,新ありな 橋本ありな
+    # "onba",  # 希志あいの
+    # "aygdk",  # 希島あいり
+    # "afib2",  # 水森翠
+    # "ayuf2",  # 園田みおん
+    # "aetua",  # 冬愛ことね
+    # "ay4sm",  # 初川南
+    # "ayua",  # JULIA
+    # "aaiqq",  # 水端あさみ
+    # "aanv6",  # 歌野こころ
+    # "aycfw",  # 辻本杏
+    # "j4wq",  # 白石もも
+    #
+    #
 ]
 
 # 爬虫名字

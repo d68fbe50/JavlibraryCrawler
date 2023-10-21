ac697f99647cc61def7c7788cc7a05d06c19220b
desonglll <lindesong666@163.com>
2023-10-21 22:00:04 +0800

    Change db_config

--- api/fast.py
+++ api/fast.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, HTTPException, Depends, Body
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Optional
 import pymysql
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 from contextlib import contextmanager
+from config.database_config import MYSQL_CONFIG as db_config
 
 # 指定模板路径
 
@@ -45,14 +47,13 @@ class ActorIDFilter(BaseModel):
 
 # 创建FastAPI应用
 app = FastAPI()
-
-# 数据库连接配置
-db_config = {
-    "host": "localhost",
-    "user": "root",
-    "password": "070011",
-    "database": "javcrawer"
-}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # 用contextmanager创建数据库连接
@@ -122,36 +123,6 @@ def fetch_all_casts():
     """
     casts_data = fetch_data_from_db("SELECT DISTINCT cast FROM works;")
     return {"casts": [cast["cast"] for cast in casts_data]}
-
-
-@app.get("/by_casts/")
-def filter_by_casts(request: Request):
-    """
-    根据演员过滤作品
-    返回一个页面，用户可以选择演员来过滤作品
-    """
-    return templates.TemplateResponse("filter_by_casts.html", {"request": request})
-
-
-@app.post("/api/works_by_cast/")
-async def get_works_by_cast(actor_id_filter: ActorIDFilter):
-    """
-    根据演员ID获取作品数据
-    输入：一个或多个演员ID
-    返回：与这些演员ID相关的作品
-    """
-    try:
-        with pymysql.connect(**db_config) as conn:
-            cursor = conn.cursor(pymysql.cursors.DictCursor)
-            query = "SELECT * FROM works WHERE actor_id IN %s"
-            cursor.execute(query, (tuple(actor_id_filter.actor_ids),))
-            works_data = cursor.fetchall()
-
-            for work in works_data:
-                work["release_date"] = work["release_date"].strftime('%Y-%m-%d')
-            return {"works": [Work(**work) for work in works_data]}
-    except Exception as e:
-        return {"error": str(e)}
 
 
 if __name__ == "__main__":
